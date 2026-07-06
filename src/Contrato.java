dc999465d8a869d77b0b7487cb3c3eb66286a710
nopercent <juliorafaelsb@gmail.com>
2026-07-06 03:04:22 -0300

    Implementação das classes endereco e contrato

--- src/Contrato.java
+++ src/Contrato.java
@@ -1,3 +1,35 @@
 public class Contrato{
+    private Cliente cliente;
+    private Imovel imovel;
+    private String tipoContrato;
+    private double valorAcordado;
 
+    public Contrato(Cliente cliente, Imovel imovel, String tipoContrato, double valorAcordado) {
+        this.cliente = cliente;
+        this.imovel = imovel;
+        this.tipoContrato = tipoContrato;
+        this.valorAcordado = valorAcordado;
+    }
+
+    // Getters
+    public Cliente getCliente() {
+        return cliente;
+    }
+    public Imovel getImovel() {
+        return imovel;
+    }
+    public String getTipoContrato() {
+        return tipoContrato;
+    }
+    public double getValorAcordado() {
+        return valorAcordado;
+    }
+
+    public String emitirContrato() {
+        return "=== CONTRATO DE " + tipoContrato.toUpperCase() + " ===\n" +
+               "Cliente: " + cliente.getNome() + " (CPF: " + cliente.getCpf() + ")\n" +
+               "Código do Imóvel:  " + imovel.getCod() + "\n" +
+               "Valor Acordado: R$ " + valorAcordado + "\n" +
+               "==========================";
+    }
 }
--- src/Endereco.java
+++ src/Endereco.java
@@ -1,3 +1,46 @@
 public class Endereco{
+    private String logradouro;
+    private int numero;
+    private String bairro;
+    private String cidade;
 
+    public Endereco(String logradouro, int numero, String bairro, String cidade) {
+        this.logradouro = logradouro;
+        this.numero = numero;
+        this.bairro = bairro;
+        this.cidade = cidade;
+    }
+
+    // Getters
+    public String getLogradouro() {
+        return logradouro;
+    }
+    public int getNumero() {
+        return numero;
+    }
+    public String getBairro() {
+        return bairro;
+    }
+    public String getCidade() {
+        return cidade;
+    }
+    
+    // Setters
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    @Override
+    public String toString() {
+        return logradouro + ", Nº " + numero + " - " + bairro + ", " + cidade;
+    }
 }
